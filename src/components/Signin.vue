33883239d629804c4473afa55cb707f2367ba60a
ikemura <k_ikemura@yumemi.co.jp>
2018-01-30 15:56:29 +0900

    サインイン処理を追加

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -3,7 +3,7 @@
     <h2>Sign in</h2>
     <input type="text" placeholder="Username" v-model="username">
     <input type="password" placeholder="Password" v-model="password">
-    <button>Signin</button>
+    <button @click="signIn">Signin</button>
     <p>You don't have an account? 
       <router-link to="/signup">create account now!!</router-link>
     </p>
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import firebase from "firebase";
 export default {
   name: "Signin",
   data: function() {
@@ -19,7 +20,22 @@ export default {
       password: ""
     };
   },
-  methods: {}
+  methods: {
+    signIn: function() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.username, this.password)
+        .then(
+          user => {
+            alert("Success!");
+            this.$router.push("/");
+          },
+          err => {
+            alert(err.message);
+          }
+        );
+    }
+  }
 };
 </script>
 
